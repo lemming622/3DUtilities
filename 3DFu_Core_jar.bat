d5e67742e30558793a17e71b4bf2a3043b361f52
kmchenry <kmchenry@d7e6b3e9-05fd-498f-94b8-1e767be53157>
2010-01-05 17:09:02 +0000

    Add jzlib for decompression in AnimatedMeshLoader_TEEVE_STREAM.

--- 3DFu_Core_jar.bat
+++ 3DFu_Core_jar.bat
@@ -1,7 +1,7 @@
-mkdir tmp\snapshot\3DFu_Core_jar\edu\ncsa\model\matrix
+mkdir tmp\snapshot\3DFu_Core_jar\edu\ncsa\model
 
 echo Mesh.class > tmp\snapshot\excludes.txt
 xcopy bin\edu\ncsa\model\*.class tmp\snapshot\3DFu_Core_jar\edu\ncsa\model /i /y /EXCLUDE:tmp\snapshot\excludes.txt
-xcopy bin\edu\ncsa\model\matrix\*.class tmp\snapshot\3DFu_Core_jar\edu\ncsa\model\matrix /i /y
+copy bin\edu\ncsa\model\AnimatedMesh.class tmp\snapshot\3DFu_Core_jar\edu\ncsa\model
 
 jar cvf tmp/snapshot/3DFu_Core.jar -C tmp/snapshot/3DFu_Core_jar .
