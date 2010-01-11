2d4bb6635ba4728c6539a6953ce80706db6cf14b
kmchenry <kmchenry@d7e6b3e9-05fd-498f-94b8-1e767be53157>
2010-01-11 18:21:24 +0000

    Fixed ModelViewerLite_jar.bat.

--- ModelViewerLite_jar.bat
+++ ModelViewerLite_jar.bat
@@ -1,13 +1,22 @@
 mkdir tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model\loaders
-mkdir tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model\matrix
 
 xcopy bin\edu\ncsa\model\ModelViewer.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
 xcopy bin\edu\ncsa\model\ModelViewerApplet.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
 xcopy bin\edu\ncsa\model\Mesh*.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
-xcopy bin\edu\ncsa\model\Utils*.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
-xcopy bin\edu\ncsa\model\ImageUtils*.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
+xcopy bin\edu\ncsa\model\AnimatedMesh*.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
 xcopy bin\edu\ncsa\model\RayTracer*.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model /i /y
-xcopy bin\edu\ncsa\model\matrix\MatrixUtils.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model\matrix /i /y
 xcopy bin\edu\ncsa\model\loaders\MeshLoader_OBJ.class tmp\snapshot\ModelViewerLite_jar\edu\ncsa\model\loaders /i /y
+
+cd tmp/snapshot/ModelViewerLite_jar
+jar xf ../../../lib/ncsa/ImageUtilities.jar
+cd ../../..
+
+cd tmp/snapshot/ModelViewerLite_jar
+jar xf ../../../lib/ncsa/MatrixUtilities.jar
+cd ../../..
+
+cd tmp/snapshot/ModelViewerLite_jar
+jar xf ../../../lib/ncsa/Utilities.jar
+cd ../../..
 
 jar cvf tmp/snapshot/ModelViewerLite.jar -C tmp/snapshot/ModelViewerLite_jar .
